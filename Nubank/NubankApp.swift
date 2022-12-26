e940333bcb61816ce0bfa7bf68b0e602ba9b24a4
Douglas Silva <silvadouglasw@gmail.com>
2022-12-26 20:15:18 -0300

    Initial Commit

---
+++ Nubank/NubankApp.swift
@@ -0,0 +1,17 @@
+//
+//  NubankApp.swift
+//  Nubank
+//
+//  Created by Douglas Silva on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NubankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
