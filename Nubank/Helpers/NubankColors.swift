2600f808e4a2e42004a2d0c551c90c7f1ce236fc
Douglas Silva <silvadouglasw@gmail.com>
2022-12-26 20:46:46 -0300

    Organize folders for header view.

---
+++ Nubank/Helpers/NubankColors.swift
@@ -0,0 +1,14 @@
+//
+//  NubankColors.swift
+//  Nubank
+//
+//  Created by Douglas Silva on 26/12/22.
+//
+
+import SwiftUI
+
+struct NubankColors {
+    static let purple = Color("NubankPurple")
+    static let lightGray = Color("NubankLightGray")
+    static let darkGray = Color("NubankDarkGray")
+}
